5cf345a57133cd6f3b9449d7c0fec45b58d8a1fe
osadasami <osadasami@gmail.com>
2022-12-19 11:19:40 +0500

    add WithData component

---
+++ src/components/WithData.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed, onMounted } from "vue";
+import Error from "./Error.vue";
+import Spinner from "./Spinner.vue";
+
+const props = defineProps<{
+  getData: () => void;
+  isLoading: boolean;
+  isError: boolean;
+}>();
+
+const hasData = computed(() => !props.isLoading && !props.isError);
+
+onMounted(() => {
+  props.getData();
+});
+</script>
+<template>
+  <Spinner :is-loading="props.isLoading" />
+  <Error :is-error="props.isError" />
+
+  <slot v-if="hasData"></slot>
+</template>
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,21 @@
 <script setup lang="ts">
 import BookList from "@/components/BookList.vue";
-import Error from "@/components/Error.vue";
 import Order from "@/components/Order.vue";
-import Spinner from "@/components/Spinner.vue";
+import WithData from "@/components/WithData.vue";
 import useBookStore from "@/stores/books";
-import { computed, onMounted } from "vue";
 
 const bookStore = useBookStore();
-const books = computed(() => bookStore.books);
-const isLoading = computed(() => bookStore.isLoading);
-const isError = computed(() => bookStore.isError);
-
-onMounted(() => {
-  bookStore.fetchBooks();
-});
 </script>
 <template>
   <div>
-    <Spinner :is-loading="isLoading" />
-    <Error :is-error="isError" />
-    <BookList v-if="!isLoading" :books="books" />
+    <WithData
+      :get-data="bookStore.fetchBooks"
+      :is-error="bookStore.isError"
+      :is-loading="bookStore.isLoading"
+    >
+      <BookList :books="bookStore.books" />
+    </WithData>
+
     <Order />
   </div>
 </template>
