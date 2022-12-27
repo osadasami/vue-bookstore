7b8ca88e2955f2ad134fe233626d7301bf185fa2
osadasami <osadasami@gmail.com>
2022-12-27 14:50:12 +0500

    add Card view

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Order from "@/components/Order.vue";
+</script>
 <template>
   <h1>Cart</h1>
+  <Order />
 </template>
 <style scoped></style>
